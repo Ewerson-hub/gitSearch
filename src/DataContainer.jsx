94c565c4637effd04b88cb11edc88e9c221db58c
Ewerson-hub <ewersonl10@gmail.com>
2026-05-08 19:24:11 -0300

    feat: implement props and recive api data

--- src/DataContainer.jsx
+++ src/DataContainer.jsx
@@ -1,63 +1,64 @@
-import { Container, Image, Link, Text, Card, Button, For, SimpleGrid, Grid, GridItem } from "@chakra-ui/react"
+import { Container, Image, Link, Text, Card, Button, For, SimpleGrid, Grid, GridItem, VStack, Spinner } from "@chakra-ui/react"
 
-const user = {
-    avatar: "https://avatars.githubusercontent.com/u/79768454?v=4",
-    name: "Ewerson Lucas",
-    repos: [
-        { name: 'gitSearch', description: 'A simple app that consumes the github api and serves some data to the front-end ', link: 'https://github.com/Ewerson-hub/gitSearch' },
-        { name: 'teste2', description: 'A CRUD app using node, handlebars and mysql with JWT authentication and bcrypto encryption. ', link: 'link2' },
-        { name: 'teste3', description: 'descrition3', link: 'link3' },
-        { name: 'teste4', description: 'descrition4', link: 'link4' },
-        { name: 'teste5', description: 'descrition5', link: 'link5' }
 
-    ],
-    url: "https://api.github.com/users/Ewerson-hub",
-    bio: "Desenvolvedor há 40 anos, Microsoft MVP, ativo contribuidor com o Stack Overflow em português. Engenheiro de formação, escovador de bit de coração."
-}
+
 const DataContainer = (props) => {
-    return (
-        <Container spaceY="20px" >
-            <Grid>
-                <GridItem justifySelf="center">
-                    <Image  src={user.avatar} alt="user.avatar" borderRadius="full" boxSize="200px" fit="cover" />
-                </GridItem>
+    if (props.data.loading === null) return <></>
 
-                <Text textStyle="2xl" fontWeight={"bold"} >
-                    {user.name}
-                </Text>
+    if (props.data.loading === true) {
+        return <VStack>
+            <Spinner />
+        </VStack>
+    } else {
+        return (
+            <Container spaceY="20px" >
+                <Grid>
+                    <GridItem justifySelf="center">
+                        <Image src={props.data.userData.avatar} alt="user.avatar" borderRadius="full" boxSize="200px" fit="cover" />
+                    </GridItem>
 
-                <Text opacity={"0.8"} textStyle="1xl" fontWeight={"light"} maxW={{base: "base",sm:"300px", md:"350px", lg:"500px"}}>
-                    {user.bio}
-                </Text>
-                
-                <Link textStyle="1xl" maxW={100} href={user.url} colorPalette="blue" fontWeight="medium" target="_blank">
-                    Github link
-                </Link>
-            </Grid>
+                    <Text textStyle="2xl" fontWeight={"bold"} >
+                        {props.data.userData.name}
+                    </Text>
 
-            <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 4 }} gap="20px">
-                <For each={user.repos}>
-                    {(repo) => (
-                        <Card.Root maxW="xs">
-                            <Card.Body gap="2">
-                                <Card.Title>{repo.name}</Card.Title>
-                                <Card.Description>
-                                    {repo.description}
-                                </Card.Description>
-                            </Card.Body>
-                            <Card.Footer>
-                                <Button variant="surface">
-                                    <Link href={repo.link} target="_blank">
-                                        Source Code
-                                    </Link>
-                                </Button>
-                            </Card.Footer>
-                        </Card.Root>
-                    )}
-                </For>
-            </SimpleGrid>
-        </Container>
-    )
+                    <Text opacity={"0.8"} textStyle="1xl" fontWeight={"light"} maxW={{ base: "base", sm: "300px", md: "350px", lg: "500px" }}>
+                        {(props.data.userData.bio) ? props.data.userData.bio : ""}
+                    </Text>
+
+                    <Link textStyle="1xl" maxW={100} href={props.data.userData.github_link} colorPalette="blue" fontWeight="medium" target="_blank">
+                        Github link
+                    </Link>
+                </Grid>
+
+                <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 4 }} gap="20px">
+                    <For each={props.data.userData.repos}>
+                        {(repo, id) => (
+                            <Card.Root maxW="xs" key={id}>
+                                <Card.Body gap="2">
+                                    <Card.Title>{repo.name}</Card.Title>
+                                    <Card.Description>
+                                        {repo.description}
+                                    </Card.Description>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <Button variant="surface">
+                                        <Link href={repo.url} target="_blank">
+                                            Source Code
+                                        </Link>
+                                    </Button>
+                                </Card.Footer>
+                            </Card.Root>
+                        )}
+                    </For>
+                </SimpleGrid>
+            </Container>
+        )
+
+    }
+
+
+
+
 }
 
 export default DataContainer
