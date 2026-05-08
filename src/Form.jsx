b2ae951c20c6782e5bf5d5158f229748380f2771
Ewerson-hub <ewersonl10@gmail.com>
2026-05-08 19:23:14 -0300

    feat: implement props and enable fetch

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,14 +1,31 @@
-import {Container, Group, IconButton, Input, Text} from "@chakra-ui/react"
+import { Container, Group, IconButton, Input, Text } from "@chakra-ui/react"
 import { LuSearch } from "react-icons/lu"
+import { getUserData } from "./reciveData"
+import { useState } from "react"
+
+
 
 const Form = (props) => {
+
+  const [input, setInput] = useState(null);
+
+  const handleSetInput = (data) => setInput(data)
+
+  const handleCallFetch = async (props) => {
+    
+    props.setUser({loading: true})
+
+    if (input && input !== "") {
+      props.setUser({userData: await getUserData(input), loading: false})
+    }
+  }
   return (
     <Container spaceY="3" centerContent gap={"0.5"}>
       <Text textStyle="6xl" fontWeight="bold">Git Search</Text>
       <Group attached>
-        <Input w="xs" placeholder="Insert a github user name"/>
+        <Input w="xs" placeholder="Insert a github user name" onChange={(e) => handleSetInput(e.target.value)} />
         <IconButton>
-          <LuSearch/>
+          <LuSearch onClick={() => handleCallFetch(props)} />
         </IconButton>
       </Group>
     </Container>
