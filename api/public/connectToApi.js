e12ab14975f44a07eaa4e094e3e7193f4b2a1ed8
Ewerson-hub <ewersonl10@gmail.com>
2026-05-10 19:04:36 -0300

    refactor: add token explication

--- api/public/connectToApi.js
+++ api/public/connectToApi.js
@@ -1,10 +1,9 @@
 import { Octokit} from "octokit";
-
+//caso queira fazer mais requisições por hora alem das 60/h padrao
+//crie um arquivo .env e crie uma variavel com nome de MY_TOKEN e adicione a sua chave de api para aumentar para 5000/h
 const api = new Octokit({
     auth: process.env.MY_TOKEN
 })
-
-
 export async function getUserInfo(user){
     return await api.request('GET /users/{username}', {
         username: user
