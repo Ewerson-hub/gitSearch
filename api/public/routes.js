d2aa8080e1f1b984b94c24eba27b88a3b324aa85
Ewerson-hub <ewersonl10@gmail.com>
2026-05-10 19:22:57 -0300

    refactor: add next middleware

--- api/public/routes.js
+++ api/public/routes.js
@@ -3,9 +3,10 @@ import { Router } from "express"
 
 const router = Router();
 
-router.get("/:id",  (req, res) => {
+router.get("/:id",  (req, res, next) => {
     res.set('Access-Control-Allow-Origin','http://localhost:5173');
     res.set('Content-type', 'aplication/json');
+    next();
 }, getUser)
 
 export default router;
